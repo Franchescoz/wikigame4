936f49a4cc8522573ab721a36434a1a9ddb3bb2c
Francisco Manuel <fcorrom2907@g.educaand.es>
2026-04-07 18:17:39 +0200

    Arreglo 3

--- middleware.js
+++ middleware.js
@@ -2,26 +2,31 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse } from 'next/server'
 
 export async function middleware(request) {
-  let response = NextResponse.next({ request })
+  let supabaseResponse = NextResponse.next({
+    request,
+  })
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
     {
       cookies: {
-        getAll() { return request.cookies.getAll() },
+        getAll() {
+          return request.cookies.getAll()
+        },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
-          response = NextResponse.next({ request })
+          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
+          supabaseResponse = NextResponse.next({
+            request,
+          })
           cookiesToSet.forEach(({ name, value, options }) =>
-            response.cookies.set(name, value, options)
+            supabaseResponse.cookies.set(name, value, options)
           )
         },
       },
     }
   )
 
- 
   const { data: { user } } = await supabase.auth.getUser()
 
   const url = request.nextUrl.clone()
@@ -35,18 +40,16 @@ export async function middleware(request) {
     return NextResponse.redirect(url)
   }
 
- 
   if (user && (pathname === '/iniciarsesion' || pathname === '/registrar')) {
     url.pathname = '/listatarjetasjuegos'
     return NextResponse.redirect(url)
   }
 
-  return response
+  return supabaseResponse
 }
 
 export const config = {
   matcher: [
-   
     '/((?!_next/static|_next/image|api|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
